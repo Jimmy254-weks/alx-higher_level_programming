7302d6a10e7d1019e9010cbf5560c19d4a1133af
Jimmy254-weks <jamesweks2019@gmail.com>
2023-09-08 11:32:48 -0700

    0x02. Python - import & modules

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
-import hidden_4
-for name in sorted(dir(hidden_4)):
-if name[:2] != '__':
-print("{}".format(name))
+    import hidden_4
+
+    names = dir(hidden_4)
+
+    for name in names:
+        if name[:2] != "__":
+            print(name)
